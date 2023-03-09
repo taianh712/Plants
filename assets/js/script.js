1a40f7fa783a02df234d0771242e1c08e8534acf
taianh712 <taianh712@gmail.com>
2023-03-09 11:02:02 +0300

    assets html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-const div = document.getElementById('navbar');
-console.log(div);
-div.addEventListener('click', () => console.log('fgfgfg'))
+hamburger = document.querySelector (".menu_mobile");
+hamburger.onclick = function(){
+    navbar =document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
 
+const accordion_headers = document.querySelectorAll(".accordion_header");
+accordion_headers.forEach(accordion_header => {
+    accordion_header.addEventListener("click", event => {
+        accordion_header.classList.toggle("active");
+    })
+})
+
+const accordion_contacts = document.querySelectorAll(".accordion_contact");
+accordion_contacts.forEach(accordion_contact => {
+    accordion_contact.addEventListener("click", event => {
+        accordion_contact.classList.toggle("active");
+    })
+})
